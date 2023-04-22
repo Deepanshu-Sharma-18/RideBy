872da834aab5a22d9e9739ded8a5f06a4fa1f0ad
Deepanshu <deepanshu.r.sharma@gmail.com>
2023-04-22 15:15:40 +0530

    Added some colors,folders and dependencies

---
+++ lib/Constants/Colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  Color electricBlue = const Color(0xFF7DF9FF);
+  Color hotPink = const Color(0xFFFF69B4);
+  Color lemonYellow = const Color(0xFFFFFF66);
+  Color brightRed = const Color(0xFFFF0000);
+  Color black = const Color(0xff020301);
+  Color backgroundWhite = const Color(0xffE8E8E8);
+}
---
+++ lib/Intro/GettingStarted/gettingstarted.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class GettingStarted extends StatefulWidget {
+  const GettingStarted({super.key});
+
+  @override
+  State<GettingStarted> createState() => _GettingStartedState();
+}
+
+class _GettingStartedState extends State<GettingStarted> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
